249d14fa75c615786dec267d5c9a78db0cda0fe8
Xiaobo <1362596047@qq.com>
2020-09-24 23:10:30 +0800

    链表

--- datastructures/src/main/java/com/pl1111w/datastructures/linkedList/ParentHeroNode.java
+++ datastructures/src/main/java/com/pl1111w/datastructures/linkedList/ParentHeroNode.java
@@ -5,17 +5,42 @@ import lombok.Data;
 @Data
 public class ParentHeroNode {
 
-    private HeroNode headNode=new HeroNode(0,"","");
+    private HeroNode headNode = new HeroNode(0, "", "");
 
     public void addNode(HeroNode node) {
         HeroNode temp = this.headNode;
         while (true) {
-            if(temp.getNext()==null){
+            if (temp.getNext() == null) {
                 break;
             }
-            temp=temp.getNext();
+            temp = temp.getNext();
         }
         temp.setNext(node);
 
+    }
+
+    public void addNodeByOrder(HeroNode heroNode) {
+        //因为头节点不能动，因此我们仍然通过一个辅助指针(变量)来帮助找到添加的位置
+        //因为单链表，因为我们找的temp 是位于 添加位置的前一个节点，否则插入不了
+        HeroNode temp = headNode;
+        boolean flag = false; // flag标志添加的编号是否存在，默认为false
+        while (true) {
+            if (temp.getNext() == null) {
+                break;
+            }
+            if (temp.getNext().getNo() > heroNode.getNo()) {
+                break;
+            } else if (temp.getNext().getNo() == heroNode.getNo()) {
+                flag = true;
+                break;
+            }
+            temp = temp.getNext();
+        }
+        if (flag) {
+            System.out.printf("准备插入的英雄的编号 %d 已经存在了, 不能加入\n", heroNode.getNo());
+        } else {
+            heroNode.setNext(temp.getNext());
+            temp.setNext(heroNode);
+        }
     }
 }
--- datastructures/src/main/java/com/pl1111w/datastructures/linkedList/SingleLinkedList.java
+++ datastructures/src/main/java/com/pl1111w/datastructures/linkedList/SingleLinkedList.java
@@ -13,11 +13,17 @@ public class SingleLinkedList {
         HeroNode node4= new HeroNode(4,"林冲","豹子头");
 
         ParentHeroNode parentHeroNode = new ParentHeroNode();
-        parentHeroNode.addNode(node2);
-        parentHeroNode.addNode(node1);
-        parentHeroNode.addNode(node3);
-        parentHeroNode.addNode(node4);
-        System.out.println(parentHeroNode.toString());
+//        parentHeroNode.addNode(node2);
+//        parentHeroNode.addNode(node1);
+//        parentHeroNode.addNode(node3);
+//        parentHeroNode.addNode(node4);
+//        System.out.println(parentHeroNode.toString());
+
+        parentHeroNode.addNodeByOrder(node2);
+        parentHeroNode.addNodeByOrder(node1);
+        parentHeroNode.addNodeByOrder(node3);
+        parentHeroNode.addNodeByOrder(node4);
+        System.out.println("顺序添加： "+parentHeroNode.toString());
     }
 
 }
