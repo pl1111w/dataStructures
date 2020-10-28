90fa9f6f478e9a10e99f6ab673d1c6be1b235786
Xiaobo <1362596047@qq.com>
2020-10-28 20:36:12 +0800

    排序算法-插入排序

---
+++ datastructures/src/main/java/com/pl1111w/datastructures/sort/InsertSort.java
@@ -0,0 +1,48 @@
+package com.pl1111w.datastructures.sort;
+
+import java.util.Arrays;
+
+/**
+ * @title: pl1111w
+ * @description: 插入排序
+ * @author: Kris
+ * @date 2020/10/28 20:10
+ */
+public class InsertSort {
+
+    /**
+     * 思路分析：
+     * 选择数组第1个元素，如果第个2元素小于第1个元素
+     * 则第一个元素后移一位,新判断的元素放到第1个元素位置
+     * 排序完成数组长度+1
+     * 继续判断第3个元素如果第三个元素小于第一个元素
+     * 则第1，2个都向后挪一位
+     * 依次类推
+     ***/
+
+    public static void main(String[] args) {
+        int arr[] = {9, 2, 3, 6, 8, 7, 5, 4, 1, 10};
+        insertSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void insertSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            int value = arr[i];
+            int j;
+            //i-1已经有序，开始排第i个元素
+            for (j = i - 1; j >= 0; j--) {
+                if (value < arr[j]) {
+                    arr[j + 1] = arr[j];//向后移一位
+                }
+                //直接跳出循环，第i个元素大于前面的i-1个元素
+                else {
+                    break;
+                }
+            }
+            //第i个元素排好
+            arr[j + 1] = value;
+        }
+    }
+}
