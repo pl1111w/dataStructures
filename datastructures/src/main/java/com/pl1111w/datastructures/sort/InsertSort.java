4836dfa22611bb69cc22a7e878626f052427547c
Xiaobo <1362596047@qq.com>
2020-11-05 21:41:24 +0800

    排序算法-插入排序

--- datastructures/src/main/java/com/pl1111w/datastructures/sort/InsertSort.java
+++ datastructures/src/main/java/com/pl1111w/datastructures/sort/InsertSort.java
@@ -13,9 +13,9 @@ public class InsertSort {
     /**
      * 思路分析：
      * 选择数组第1个元素，如果第个2元素小于第1个元素
-     * 则第一个元素后移一位,新判断的元素放到第1个元素位置
-     * 排序完成数组长度+1
-     * 继续判断第3个元素如果第三个元素小于第一个元素
+     * 则第1个元素后移一位,第2个元素放到第1个元素位置
+     * 否则直接放到第1个元素后面，此时排序完成数组长度+1
+     * 继续判断第3个元素如果第3个元素小于第1个元素
      * 则第1，2个都向后挪一位
      * 依次类推
      ***/
@@ -29,20 +29,17 @@ public class InsertSort {
     private static void insertSort(int[] arr) {
 
         for (int i = 1; i < arr.length; i++) {
-            int value = arr[i];
+            int temp = arr[i];
             int j;
-            //i-1已经有序，开始排第i个元素
             for (j = i - 1; j >= 0; j--) {
-                if (value < arr[j]) {
-                    arr[j + 1] = arr[j];//向后移一位
-                }
-                //直接跳出循环，第i个元素大于前面的i-1个元素
-                else {
-                    break;
+
+                if (arr[j] > temp) {
+                    arr[j + 1] = arr[j];//挪位置
+                } else {
+                    break;//跳出循环，开始插入下一个数
                 }
             }
-            //第i个元素排好
-            arr[j + 1] = value;
+            arr[j] = temp;
         }
     }
 }
