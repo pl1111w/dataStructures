ae61c33d0c5e2483a83db877e1d0476ae5c1c748
Xiaobo <1362596047@qq.com>
2020-11-19 22:18:50 +0800

    ArrBinaryTree

--- datastructures/src/main/java/com/pl1111w/datastructures/tree/ArrBinaryTreeDemo.java
+++ datastructures/src/main/java/com/pl1111w/datastructures/tree/ArrBinaryTreeDemo.java
@@ -13,7 +13,8 @@ public class ArrBinaryTreeDemo {
         //创建一个 ArrBinaryTree
         ArrBinaryTree arrBinaryTree = new ArrBinaryTree(arr);
         // arrBinaryTree.preOrder(); // 1,2,4,5,3,6,7
-        arrBinaryTree.midOrder();//4,2,5,1,6,3,7
+        //arrBinaryTree.midOrder();//4,2,5,1,6,3,7
+        arrBinaryTree.afterOrder();//4,5,2,6,7,3,1
     }
 }
 
@@ -30,6 +31,10 @@ class ArrBinaryTree {
 
     public void midOrder() {
         this.midOrder(0);
+    }
+
+    public void afterOrder() {
+        this.afterOrder(0);
     }
 
     private void preOrder(int i) {
@@ -69,5 +74,21 @@ class ArrBinaryTree {
 
     }
 
-
+    private void afterOrder(int i) {
+        if (arr == null) {
+            System.out.println("数组为空，不能按照二叉树的前序遍历");
+            return;
+        }
+        if (arr.length == 0) {
+            System.out.println("数组为空，不能按照二叉树的前序遍历");
+            return;
+        }
+        if (i * 2 + 1 < arr.length) {
+            afterOrder(i * 2 + 1);
+        }
+        if (i * 2 + 2 < arr.length) {
+            afterOrder(i * 2 + 2);
+        }
+        System.out.println(arr[i]);
+    }
 }
