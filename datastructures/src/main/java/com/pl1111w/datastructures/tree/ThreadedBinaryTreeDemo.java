c88ee1dfd40d9075726c7c9ec45a67ade978c829
Xiaobo <1362596047@qq.com>
2020-11-21 15:46:00 +0800

    ThreadBinaryTree-线索二叉树

--- datastructures/src/main/java/com/pl1111w/datastructures/tree/ThreadedBinaryTreeDemo.java
+++ datastructures/src/main/java/com/pl1111w/datastructures/tree/ThreadedBinaryTreeDemo.java
@@ -12,7 +12,7 @@ public class ThreadedBinaryTreeDemo {
 
     public static void main(String[] args) {
 
-        ThreadHeroNode root = new ThreadHeroNode(1, "java");
+        ThreadHeroNode root = new ThreadHeroNode(1, "Java");
         ThreadHeroNode node2 = new ThreadHeroNode(3, "C#");
         ThreadHeroNode node3 = new ThreadHeroNode(6, "Python");
         ThreadHeroNode node4 = new ThreadHeroNode(8, "C++");
@@ -26,23 +26,31 @@ public class ThreadedBinaryTreeDemo {
         node2.setRight(node5);
         node3.setLeft(node6);
 
-        System.out.println("==========中序线索化开始=============");
-        System.out.println("｛8, 3, 10, 1, 14, 6｝");
+//        System.out.println("==========中序线索化开始=============");
+//        System.out.println("｛8, 3, 10, 1, 14, 6｝");
         ThreadedBinaryTree threadedBinaryTree = new ThreadedBinaryTree();
         threadedBinaryTree.setRoot(root);
-        threadedBinaryTree.midThreadedNodes();
-        ThreadHeroNode leftNode = node5.getLeft();
-        ThreadHeroNode rightNode = node5.getRight();
-        System.out.println("10号结点的前驱结点是 =" + leftNode); //3
-        System.out.println("10号结点的后继结点是=" + rightNode); //1
+//        threadedBinaryTree.midThreadedNodes();
+//        ThreadHeroNode leftNode = node5.getLeft();
+//        ThreadHeroNode rightNode = node5.getRight();
+//        System.out.println("10号结点的前驱结点是 =" + leftNode); //3
+//        System.out.println("10号结点的后继结点是=" + rightNode); //1
+//
+//        System.out.println("==========前序线索化开始=============");
+//        System.out.println("｛1，3，8，10，6，14｝");
+//        threadedBinaryTree.preThreadedNodes();
+//        ThreadHeroNode preLeftNode = node4.getLeft();
+//        ThreadHeroNode preRightNode = node4.getRight();
+//        System.out.println("8号结点的前驱结点是 =" + preLeftNode); //3
+//        System.out.println("8号结点的后继结点是=" + preRightNode); //10
 
-        System.out.println("==========前序线索化开始=============");
-        System.out.println("｛1，3，8，10，6，14｝");
-        threadedBinaryTree.preThreadedNodes();
-        ThreadHeroNode preLeftNode = node4.getLeft();
-        ThreadHeroNode preRightNode = node4.getRight();
-        System.out.println("8号结点的前驱结点是 =" + preLeftNode); //3
-        System.out.println("8号结点的后继结点是=" + preRightNode); //10
+        System.out.println("==========后序线索化开始=============");
+        System.out.println("｛8，10，3, 14, 6，1｝");
+        threadedBinaryTree.afterThreadedNodes();
+        ThreadHeroNode afterLeftNode = node6.getLeft();
+        ThreadHeroNode afterRightNode = node6.getRight();
+        System.out.println("14号结点的前驱结点是 =" + afterLeftNode); //1
+        System.out.println("14号结点的后继结点是=" + afterRightNode); //6
 
     }
 }
@@ -155,5 +163,30 @@ class ThreadedBinaryTree {
             preThreadedNodes(node.getRight());
         }
 
+    }
+
+    public void afterThreadedNodes() {
+        this.afterThreadedNodes(root);
+    }
+
+    private void afterThreadedNodes(ThreadHeroNode node) {
+        //如果node==null, 不能线索化
+        if (node == null) {
+            return;
+        }
+        afterThreadedNodes(node.getLeft());
+        afterThreadedNodes(node.getRight());
+
+        if (node.getLeft() == null) {
+            node.setLeftType(1);
+            node.setLeft(pre);
+        }
+        if (pre != null && pre.getRight() == null) {
+            //让前驱结点的右指针指向当前结点
+            pre.setRight(node);
+            //修改前驱结点的右指针类型
+            pre.setRightType(1);
+        }
+        pre = node;
     }
 }
