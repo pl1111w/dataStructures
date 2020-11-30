504934ca117ef6cdd0253aed7081518338ff4152
Xiaobo <1362596047@qq.com>
2020-11-30 22:28:14 +0800

    图

---
+++ datastructures/src/main/java/com/pl1111w/datastructures/graph/Graph.java
@@ -0,0 +1,142 @@
+package com.pl1111w.datastructures.graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @title: pl1111w
+ * @description: 图
+ * @author: Kris
+ * @date 2020/11/30 21:20
+ */
+public class Graph {
+
+    private ArrayList<String> vertexList;//存储顶点集合
+
+    private int[][] edges;//存储邻接矩阵
+
+    private int numOfEdges;//表示边的数目
+
+    //定义给数组boolean[], 记录某个结点是否被访问
+    private boolean[] isVisited;
+
+    public static void main(String[] args) {
+        int n = 5;  //结点的个数
+        String vertexes[] = {"A", "B", "C", "D", "E"};
+        //创建图对象
+        Graph graph = new Graph(n);
+        //循环的添加顶点
+        for (String vertex : vertexes) {
+            graph.insertVertex(vertex);
+        }
+        //添加边
+        //A-B A-C B-C B-D B-E
+        graph.insertEdge(0, 1, 1); // A-B
+        graph.insertEdge(0, 2, 1); //
+        graph.insertEdge(1, 2, 1); //
+        graph.insertEdge(1, 3, 1); //
+        graph.insertEdge(1, 4, 1); //
+
+        //显示一把邻结矩阵
+        graph.showGraph();
+
+        graph.dfs();
+
+    }
+
+    private void dfs() {
+        for (int i = 0; i < vertexList.size(); i++) {
+            if (!isVisited[i]) {
+                dfs(isVisited, i);
+            }
+        }
+
+    }
+
+    private void dfs(boolean[] isVisited, int i) {
+        //首先我们访问该结点,输出
+        System.out.print(getValueByIndex(i) + "->");
+        //将结点设置为已经访问
+        isVisited[i] = true;
+        //查找结点i的第一个邻接结点w
+        int w = getFirstNeighbor(i);
+        while (w != -1) {
+            if (!isVisited[w]) {
+                dfs(isVisited, w);
+            }
+            //如果w结点已经被访问过
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+    //根据前一个邻接结点的下标来获取下一个邻接结点
+    private int getNextNeighbor(int v1, int v2) {
+        for (int j = v2 + 1; j < vertexList.size(); j++) {
+            if (edges[v1][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    private int getFirstNeighbor(int index) {
+        for (int j = 0; j < vertexList.size(); j++) {
+            if (edges[index][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    public Graph(int n) {
+        vertexList = new ArrayList<>(n);
+        edges = new int[n][n];
+        numOfEdges = 0;
+        isVisited = new boolean[n];
+    }
+
+
+    /**
+     * @param v1     表示点的下标即使第几个顶点  "A"-"B" "A"->0 "B"->1
+     * @param v2     第二个顶点对应的下标
+     * @param weight 表示
+     */
+    //添加边
+    public void insertEdge(int v1, int v2, int weight) {
+        edges[v1][v2] = weight;
+        edges[v2][v1] = weight;
+        numOfEdges++;
+    }
+
+    //返回结点的个数
+    public int getNumOfVertex() {
+        return vertexList.size();
+    }
+
+    //显示图对应的矩阵
+    public void showGraph() {
+        for (int[] link : edges) {
+            System.err.println(Arrays.toString(link));
+        }
+    }
+
+    //得到边的数目
+    public int getNumOfEdges() {
+        return numOfEdges;
+    }
+
+    //返回结点i(下标)对应的数据 0->"A" 1->"B" 2->"C"
+    public String getValueByIndex(int i) {
+        return vertexList.get(i);
+    }
+
+    //返回v1和v2的权值
+    public int getWeight(int v1, int v2) {
+        return edges[v1][v2];
+    }
+
+    //插入结点
+    public void insertVertex(String vertex) {
+        vertexList.add(vertex);
+    }
+}
