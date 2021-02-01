4f508936feaf75db6384b57f59c6ca3f02f240ce
Xiaobo <1362596047@qq.com>
2021-02-01 23:01:37 +0800

    右视图-树

--- datastructures/src/main/java/com/pl1111w/datastructures/tree/leeCode/RightViewOfTree.java
+++ datastructures/src/main/java/com/pl1111w/datastructures/tree/leeCode/RightViewOfTree.java
@@ -13,6 +13,8 @@ import java.util.Queue;
  */
 public class RightViewOfTree {
 
+    private static List<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) {
         TreeNode parent = new TreeNode(5);
         TreeNode left = new TreeNode(3);
@@ -26,7 +28,22 @@ public class RightViewOfTree {
         parent.setLeft(left);
         parent.setRight(right);
 
-        List<Integer> list = rightViewOfTree(parent);
+        rightViewOfTree(parent);
+        rightViewOfTree1(parent, 0);
+    }
+
+    private static List<Integer> rightViewOfTree1(TreeNode parent, int depth) {
+        if (parent == null) {
+            return list;
+        }
+        // 如果当前节点所在深度还没有出现在res里，说明在该深度下当前节点是第一个被访问的节点，因此将当前节点加入res中。
+        if (depth == list.size()) {
+            list.add(parent.val);
+        }
+        depth++;
+        rightViewOfTree1(parent.right, depth);
+        rightViewOfTree1(parent.left, depth);
+        return list;
     }
 
     private static List<Integer> rightViewOfTree(TreeNode node) {
